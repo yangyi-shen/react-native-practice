4f486636c51f0f9928e16928d00e8802f82e75e0
Yang-Yi Shen <mrshenyangyi@gmail.com>
2026-04-09 15:32:55 +0700

    feat: cá nhân hóa thanh điều hướng

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,26 +1,65 @@
+import MaterialIcons from "@expo/vector-icons/MaterialIcons";
 import { Tabs } from "expo-router";
+import { StyleSheet } from "react-native";
 
 export default function TabLayout() {
+  const tabBarIconSize = 32;
+
   return (
-    <Tabs>
+    <Tabs
+      screenOptions={{
+        tabBarShowLabel: false,
+        headerShown: false,
+      }}
+    >
       <Tabs.Screen
         name="index"
         options={{
           title: "Trang chủ",
+          tabBarIcon: ({ color }) => (
+            <MaterialIcons
+              style={styles.tabBarIcon}
+              name="home"
+              size={tabBarIconSize}
+              color={color}
+            />
+          ),
         }}
       />
       <Tabs.Screen
         name="gallery"
         options={{
           title: "Trang trình bày",
+          tabBarIcon: ({ color }) => (
+            <MaterialIcons
+              style={styles.tabBarIcon}
+              name="dashboard"
+              size={tabBarIconSize}
+              color={color}
+            />
+          ),
         }}
       />
       <Tabs.Screen
         name="contact"
         options={{
           title: "Liên hệ",
+          tabBarIcon: ({ color }) => (
+            <MaterialIcons
+              style={styles.tabBarIcon}
+              name="call"
+              size={tabBarIconSize}
+              color={color}
+            />
+          ),
         }}
       />
     </Tabs>
   );
 }
+
+const styles = StyleSheet.create({
+  tabBarIcon: {
+    height: "100%",
+  },
+});
