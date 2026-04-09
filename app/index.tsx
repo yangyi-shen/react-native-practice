78b414c67aa0a54c17db822a0ab066fd863e296c
Yang-Yi Shen <mrshenyangyi@gmail.com>
2026-04-09 14:05:26 +0700

    feat: hoàn tập trang chủ

--- app/index.tsx
+++ app/index.tsx
@@ -1,9 +1,28 @@
-import { StyleSheet, Text, View } from "react-native";
+import { ThemeColors, ThemeSpacing, ThemeTypography } from "@/constants/theme";
+import { Image } from "expo-image";
+import { ScrollView, StyleSheet, Text, View } from "react-native";
+
+import trungVitLonImg from "@/assets/images/trung-vit-lon.png";
 
 export default function Index() {
   return (
     <View style={styles.main}>
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Image style={styles.heroImage} source={trungVitLonImg} />
+      <ScrollView>
+        <View style={styles.headerView}>
+          <Text style={ThemeTypography.title}>
+            Chào mừng vào app fan cho món Trứng Vịt Lộn!
+          </Text>
+        </View>
+        <View style={styles.bodyView}>
+          <Text style={ThemeTypography.default}>
+            Trứng vịt lộn là một món ăn thôm ngon được làm từ trứng vịt mà ở
+            trong còn có vịt. Món này rất phỏ biến trong rất nhiều quốc gia,
+            nhưng chỉ có bản được chuẩn bị trong miền nam Việt Nam mới là ngon
+            nhất.
+          </Text>
+        </View>
+      </ScrollView>
     </View>
   );
 }
@@ -11,7 +30,17 @@ export default function Index() {
 const styles = StyleSheet.create({
   main: {
     flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
+    backgroundColor: ThemeColors.background,
+  },
+  heroImage: {
+    width: "100%",
+    aspectRatio: 2,
+  },
+  headerView: {
+    paddingHorizontal: ThemeSpacing.spacing_4,
+    paddingVertical: ThemeSpacing.spacing_2,
+  },
+  bodyView: {
+    paddingHorizontal: ThemeSpacing.spacing_4,
   },
 });
