64e33793d314a512a0e96c5416eef2a56b579a25
Yang-Yi Shen <mrshenyangyi@gmail.com>
2026-04-09 13:54:14 +0700

    feat: tạo trang trình bày

--- app/gallery.tsx
+++ app/gallery.tsx
@@ -1,9 +1,22 @@
+import { GalleryFlatList } from "@/components/gallery-flat-list";
+import { ThemeColors, ThemeSpacing, ThemeTypography } from "@/constants/theme";
 import { StyleSheet, Text, View } from "react-native";
 
 export default function Gallery() {
   return (
     <View style={styles.main}>
-      <Text>Đây là trang trình bày ảnh.</Text>
+      <View style={styles.headerView}>
+        <Text style={ThemeTypography.title}>
+          Trang trình bày ảnh trứng vịt lộn
+        </Text>
+      </View>
+      <View style={styles.bodyView}>
+        <Text style={ThemeTypography.default}>
+          Ở đậy, bạn có thể ngắm và hưởng thức những bước ảnh của nhiều loại
+          trứng vịt lộn.
+        </Text>
+      </View>
+      <GalleryFlatList />
     </View>
   );
 }
@@ -11,7 +24,13 @@ export default function Gallery() {
 const styles = StyleSheet.create({
   main: {
     flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
+    backgroundColor: ThemeColors.background,
+  },
+  headerView: {
+    paddingHorizontal: ThemeSpacing.spacing_4,
+    paddingVertical: ThemeSpacing.spacing_2,
+  },
+  bodyView: {
+    paddingHorizontal: ThemeSpacing.spacing_4,
   },
 });
