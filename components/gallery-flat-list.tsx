2fe5407589721ec4e1663de6958dafec1bd77bcd
Yang-Yi Shen <mrshenyangyi@gmail.com>
2026-04-09 13:54:05 +0700

    feat: tạo component khu trình bày

---
+++ components/gallery-flat-list.tsx
@@ -0,0 +1,44 @@
+import { ThemeSpacing } from "@/constants/theme";
+import { FlatList, StyleSheet } from "react-native";
+import { GalleryImage } from "./gallery-image";
+
+import gallery1Img from "@/assets/images/gallery/gallery-1.jpg";
+import gallery2Img from "@/assets/images/gallery/gallery-2.jpg";
+import gallery3Img from "@/assets/images/gallery/gallery-3.jpg";
+import gallery4Img from "@/assets/images/gallery/gallery-4.jpg";
+import gallery5Img from "@/assets/images/gallery/gallery-5.jpg";
+import gallery6Img from "@/assets/images/gallery/gallery-6.jpg";
+
+export function GalleryFlatList() {
+  const galleryImages = [
+    gallery1Img,
+    gallery2Img,
+    gallery3Img,
+    gallery4Img,
+    gallery5Img,
+    gallery6Img,
+  ];
+
+  return (
+    <FlatList
+      style={styles.galleryFlatList}
+      contentContainerStyle={styles.galleryFlatListGapStyle}
+      columnWrapperStyle={styles.galleryFlatListGapStyle}
+      data={galleryImages}
+      numColumns={2}
+      renderItem={({ item }) => <GalleryImage source={item} />}
+    />
+  );
+}
+
+const styles = StyleSheet.create({
+  galleryFlatList: {
+    paddingHorizontal: ThemeSpacing.spacing_4,
+    paddingVertical: ThemeSpacing.spacing_2,
+    gap: ThemeSpacing.spacing_2,
+    /* gap handled by galleryFlatListGapStyle */
+  },
+  galleryFlatListGapStyle: {
+    gap: ThemeSpacing.spacing_2,
+  },
+});
